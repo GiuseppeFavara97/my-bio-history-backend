bce9c9c10a55cad3baf4b44a9b1cfb86e8db1ef5
GiuseppeFavara97 <giuseppe.favara1997@gmail.com>
2025-06-14 12:11:41 +0200

    Refactor main.ts to add global exception filter and set API prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.setGlobalPrefix('api');
+  await app.listen(3000);
+  console.log('Application running on http://localhost:3000/api');
 }
 bootstrap();
