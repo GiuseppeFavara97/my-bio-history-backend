c629f2ab9619a93dcaecb3c37bde7e2590c1df29
michele <michele@beehive@gmail.com>
2025-06-19 11:38:06 +0200

    update dto

--- backend/src/medical_records/dto/medical.dto.ts
+++ backend/src/medical_records/dto/medical.dto.ts
@@ -1,14 +1,9 @@
-import { IsNotEmpty, IsDate } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class MedicalDto {
     @IsNotEmpty()
     id_patient:number;
     
-
-    @IsDate()
-    created_at:Date;
-    deleted_at:Date;
-
     
 
 }
