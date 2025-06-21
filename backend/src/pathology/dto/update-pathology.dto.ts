ce35ae794d833875f7514b88d34593d5cbf5259e
GiuseppeFavara97 <giuseppe.favara1997@gmail.com>
2025-06-21 16:38:25 +0200

    remove unused care and diagnosis modules, services, controllers, and DTOs

--- backend/src/pathology/dto/update-pathology.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePathologyDto } from './create-pathology.dto';
-
-export class UpdatePathologyDto extends PartialType(CreatePathologyDto) {}
