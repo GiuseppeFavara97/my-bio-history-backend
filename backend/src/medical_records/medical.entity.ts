bc96f2af890b062f672ec05b6bfe14a2ff8cc37a
Alex-debug-sudo <alessandrocaruso2194@gmail.com>
2025-06-24 11:17:46 +0200

    foreign Key sistemate da vito

--- backend/src/medical_records/medical.entity.ts
+++ backend/src/medical_records/medical.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, DeleteDateColumn, UpdateDateColumn} from "typeorm";
+import { Diagnosis } from "src/diagnoses/diagnosis.entity";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, DeleteDateColumn, UpdateDateColumn, OneToMany} from "typeorm";
 
 @Entity("medical_records")
 export class Medical_Records {
@@ -11,12 +12,14 @@ export class Medical_Records {
     @CreateDateColumn()
     created_At: Date;
 
-   @DeleteDateColumn()
+    @DeleteDateColumn()
     deleted_At: Date;
 
     @UpdateDateColumn()
     update:Date;
 
+    @OneToMany(() => Diagnosis, (diagnosis) => diagnosis.medicalrecord)
+    diagnoses: Diagnosis[]
 }
 
 
