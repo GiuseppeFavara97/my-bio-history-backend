876cf9fc901cba0be00642e20f7f95e8ecb0f414
Giuseppe Favara <164042127+GiuseppeFavara97@users.noreply.github.com>
2025-06-27 22:13:18 +0200

    Delete backend/src/vaccines/dto/create-vaccine.dto.ts

--- backend/src/vaccines/dto/create-vaccine.dto.ts
+++
@@ -1,9 +0,0 @@
-
-import { IsNotEmpty, IsDate } from 'class-validator';
-export class CreateVaccineDto {   
-    @IsNotEmpty()
-    vaccine_name: string;
-    
-    @IsDate()
-    vaccine_date: Date;    
-}
