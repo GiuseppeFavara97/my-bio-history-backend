c1daa6995ac48c42621bf304647c5de1c91a3885
GiuseppeFavara97 <giuseppe.favara1997@gmail.com>
2025-07-03 10:54:48 +0200

    fixed error conflict user

---
+++ backend/src/users/dto/user.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsEmail } from 'class-validator';
+
+export class UserDto {
+    @IsNotEmpty()
+    username: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    password: string;
+}
---
+++ backend/src/users/user.entity.ts
@@ -0,0 +1,41 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity("users")
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column({ nullable: true })
+    firstName: string;
+
+    @Column({ nullable: true })
+    lastName: string;
+
+    @Column({ nullable: true })
+    birthday: Date;
+
+    @Column({ nullable: true })
+    birthday_place: string;
+
+    @Column({ nullable: true })
+    province: string;
+
+    @Column({ type: 'char', length: 1, nullable: true })
+    sex: 'M' | 'F' | null;
+
+    @Column({ nullable: true })
+    phoneNumber: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+}
