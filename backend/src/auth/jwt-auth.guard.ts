68b5b2d7b0f290bc9e0ee5f2000e9046208004ea
michele <michele@beehive@gmail.com>
2025-07-07 13:43:40 +0200

    fixed authentication

---
+++ backend/src/auth/jwt-auth.guard.ts
@@ -0,0 +1,26 @@
+
+import {
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './auth.decorator';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JWTAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+    context.getHandler(),
+    context.getClass(),
+    ]);
+    if(isPublic) {
+      return true;
+    }
+      return super.canActivate(context);
+  }
+}
