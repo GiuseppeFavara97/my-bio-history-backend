8326e36f2fe5482f71e3f77f13737d9055113bfe
GiuseppeFavara97 <giuseppe.favara1997@gmail.com>
2025-08-03 15:54:01 +0200

    add dependency

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,47 +7,24 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   // Abilita CORS per permettere al frontend di comunicare
   app.enableCors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  // permette di accettare richieste con credenziali e quindi token (aggiunta perchè non accettava il token il frontend)
-});
+    origin: [
+      'http://localhost:3000',
+      'https://my-bio-history-frontend.vercel.app'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    // permette di accettare richieste con credenziali e quindi token (aggiunta perchè non accettava il token il frontend)
+  });
 
   app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('api');
-  await app.listen(3001);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 
-  console.log('Application running on http://localhost:3001/api');
+  console.log(`Application running on http://localhost:${port}/api`);
 }
 bootstrap();
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.setGlobalPrefix('api');
-  await app.listen(3001);
-  console.log('Application running on http://localhost:3000/api');
-}
-bootstrap();
-*/
