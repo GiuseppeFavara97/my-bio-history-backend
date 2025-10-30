7fbeedc570cd230baacc077e04be4fc676959092
Alex-debug-sudo <alessandrocaruso2194@gmail.com>
2025-10-30 12:49:47 +0100

    modified endpoint for update WIP

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     // permette di accettare richieste con credenziali e quindi token (aggiunta perchè non accettava il token il frontend)
   });
